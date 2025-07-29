bb807cdcf86c588600620b3ba5a3481b13439342
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-07-29 17:45:35 -0400

    wip: implement failed test for more complicated beta redux

--- python/test_beta.py
+++ python/test_beta.py
@@ -9,6 +9,10 @@ N = new_name
 identity = F(N(0))
 self_apply = F(A(N(0), N(0)))
 
+# I want to call this simply "apply", but Python has already taken the
+# name.
+apply_fn = F(F(A(N(1), N(0))))
+
 
 class TestBetaReduction(unittest.TestCase):
     def test_identity(self):
@@ -17,3 +21,11 @@ class TestBetaReduction(unittest.TestCase):
         reduced_term = beta_reduce(term)
 
         self.assertEqual(reduced_term, self_apply)
+
+    def test_2_10_example(self):
+        term = A(F(A(N(0), identity)),
+                 self_apply)
+
+        reduced_term = beta_reduce(term)
+
+        self.assertEqual(reduced_term, identity)
