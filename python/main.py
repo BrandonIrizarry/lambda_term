303b41af3c12eeb3b115b31a2b50acaed0694f39
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-07-29 21:01:42 -0400

    Add textual introduction to REPL on first launch

--- python/main.py
+++ python/main.py
@@ -1,12 +1,31 @@
 from beta import beta_reduce
 from parse import parse
 
+print("""
+Welcome to the lambda REPL.
+
+You can enter an lambda term at the prompt to see its result under
+normal order beta reduction.
+
+This works for all lambda terms, though the result is interesting
+normally only for application terms.
+
+Use backslash (\\) for the lambda symbol.
+
+All lambda application terms must be explicitly parenthesized.
+
+For example, for the self-application function, the correct form would
+be \\x.(x x), not \\x.x x.
+""")
+
 while True:
     try:
         raw_term = input("> ")
         ast, _ = parse(raw_term)
 
         value = beta_reduce(ast)
+
+        print()
         print(value)
     except EOFError:
         break
