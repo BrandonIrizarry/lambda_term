6bd50ecbaf1f8597f915266f99d18dd0b9376056
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-07-29 20:49:00 -0400

    Print beta-reduxed AST as REPL eval+print step

--- python/main.py
+++ python/main.py
@@ -1,9 +1,12 @@
 from beta import beta_reduce
+from parse import parse
 
 while True:
     try:
         raw_term = input("> ")
+        ast, _ = parse(raw_term)
 
-        print(raw_term)
+        value = beta_reduce(ast)
+        print(value)
     except EOFError:
         break
