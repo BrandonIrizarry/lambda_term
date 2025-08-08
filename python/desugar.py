f35cf79c8d2830d3cf2f16b86da58cbdcd1fb101
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-08 11:01:46 -0400

    Move 'desugar_def' to desugar.py
    Hopefully, this will work out as an entire module for desugaring (let,
    if, match, and so on)

---
+++ python/desugar.py
@@ -0,0 +1,34 @@
+import error as err
+import tokenize_lambda as tkz
+
+
+def desugar_def(tokens):
+    if not tkz.is_def_t(tokens[0]):
+        return tokens, None
+
+    second_token = tokens[1]
+
+    if not tkz.is_name_t(second_token):
+        raise err.IllegalTokenError(1, second_token)
+
+    label = second_token["value"]
+
+    # Move the left-side params to the right side as lambda
+    # binders.
+    mid = tkz.find(tokens, tkz.assign_t())
+
+    if mid == -1:
+        raise err.MissingAssignmentError
+
+    params = tokens[2:mid]
+    body = tokens[mid+1:]
+
+    fn_prefix = []
+
+    for p in params:
+        for t in [tkz.lambda_t(), p, tkz.dot_t()]:
+            fn_prefix.append(t)
+
+    tokens = fn_prefix + body
+
+    return tokens, label
