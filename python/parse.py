c9b595030528c25b6e0365fc445e7c6762ca5180
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-07 22:03:16 -0400

    Fix really nasty type-related bug
    I was wrapping a token around an existing token. I'm starting to see
    where static types can really come in handy. :)

--- python/parse.py
+++ python/parse.py
@@ -172,7 +172,7 @@ def parse(raw_term, genv):
         params = tokens[2:mid]
         body = tokens[mid+1:]
         fn_prefix = list(itertools.chain.from_iterable(
-            [[tkz.lambda_t(), tkz.name_t(p), tkz.dot_t()] for p in params]))
+            [[tkz.lambda_t(), p, tkz.dot_t()] for p in params]))
 
         tokens = fn_prefix + body
 
