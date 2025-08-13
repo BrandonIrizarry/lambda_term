afff496713ea227a0a599ba8fb4b62ff80e45fd7
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-12 22:05:36 -0400

    test: add test using the loads-only file

--- python/tests/test_directive.py
+++ python/tests/test_directive.py
@@ -1,6 +1,14 @@
 import unittest
 
 import directive as dtv
+import evaluate as evl
+import term
+
+N = term.new_name
+F = term.new_abstraction
+A = term.new_application
+
+zero = F(N(0))
 
 
 class TestLoad(unittest.TestCase):
@@ -12,3 +20,16 @@ class TestLoad(unittest.TestCase):
         program = status["user_data"]
 
         self.assertEqual(len(program), 13)
+
+    def test_load_inside_code_file(self):
+        status = dtv.eval_directive("load", ["tests/prelude_plus_mz"])
+
+        self.assertIsNone(status["error"])
+
+        program = status["user_data"]
+
+        status = evl.eval_program(program, [])
+
+        self.assertIsNone(status["error"])
+
+        self.assertEqual(status["user_data"], zero)
