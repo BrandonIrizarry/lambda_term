b251cb6ad91fb82fde452d57e18479f214a99389
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-17 21:06:04 -0400

    feat: implement free-symbol entries in gamma with AST field
    This commit also introduces some helper functions.

--- lbd/gamma.py
+++ lbd/gamma.py
@@ -1,5 +1,15 @@
-# For now, implement gamma as a list of free names, given as strings.
-_gamma: list[str] = []
+from typing import TypedDict
+
+from lbd.term import AST
+
+
+class Symbol(TypedDict):
+    """Associate a symbol name with its definition."""
+    name: str
+    ast: AST | None
+
+
+_gamma: list[Symbol] = []
 
 
 def gamma(free_name: str) -> int | None:
@@ -9,32 +19,77 @@ def gamma(free_name: str) -> int | None:
 
     """
 
-    try:
-        index = _gamma.index(free_name)
-    except ValueError:
-        return None
+    for i in range(len(_gamma)):
+        n = _gamma[i]["name"]
 
-    return index
+        if n == free_name:
+            return i
+
+    return None
 
 
-def add(free_name: str) -> None:
+def sym_declare(free_name: str) -> None:
     """Add FREE_NAME to gamma."""
 
-    _gamma.append(free_name)
+    new_symbol: Symbol = {"name": free_name, "ast": None}
+    _gamma.append(new_symbol)
 
 
-def debruijn(free_name: str, offset: int) -> int:
+def sym_set(sym_name: str, ast: AST | None, delete: bool = False) -> bool:
+    """Set the definition of SYM_NAME to AST.
+
+    An AST of 'None' instructs to clear the definition; the DELETE
+    flag instructs to remove the free symbol entirely from _gamma.
+
+    Return whether the symbol was found.
+
+    """
+
+    found = False
+
+    for i in range(len(_gamma)):
+        if _gamma[i]["name"] == sym_name:
+            if delete:
+                del _gamma[i]
+            else:
+                _gamma[i]["ast"] = ast
+
+            found = True
+
+    return found
+
+
+def sym_clear(sym_name: str) -> bool:
+    """Clear the AST-defintion of SYM_NAME.
+
+    Return whether the symbol was found.
+
+    """
+
+    return sym_set(sym_name, None)
+
+
+def sym_delete(sym_name: str) -> bool:
+    """Remove SYM_NAME's status as a free symbol.
+
+    Return whether the symbol was found.
+
+    """
+
+    return sym_set(sym_name, None, True)
+
+
+def debruijn(free_name: str, offset: int) -> int | None:
     """Compute the effective DeBruijn index of FREE_NAME.
 
-    OFFSET is meant to be the length of the current local, nameless
-    environment kept track of by the parser.
+    OFFSET is meant to be the length of the current local environment
+    kept track of by the parser.
 
     """
 
     index = gamma(free_name)
 
-    if index == -1:
-        add(free_name)
-        index = gamma(free_name)
+    if index is None:
+        return index
 
     return offset + index
