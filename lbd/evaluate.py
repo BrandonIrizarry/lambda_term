59548b216f3f888a8ca8cf3e5312333fc0af4bad
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-19 20:23:54 -0400

    refactor: remove eval-line and eval-program

--- lbd/evaluate.py
+++ lbd/evaluate.py
@@ -1,7 +1,4 @@
-import re
-
 import lbd.beta as beta
-import lbd.directive as dtv
 import lbd.error as err
 import lbd.parse as parse
 import lbd.tokenize_lambda as tkz
@@ -26,57 +23,3 @@ def eval_raw_term(raw_term: str) -> AST | LambdaError:
         return err.error(tokens, num_tokens, err.Err.TRAILING_GARBAGE)
 
     return beta.beta_reduce(ast)
-
-
-def eval_program(program: list[str]) -> AST | LambdaError:
-    value = dict()
-
-    for line in program:
-        ast = eval_line(line)
-
-        if isinstance(ast, LambdaError):
-            return ast
-
-        value = ast
-
-    return value
-
-
-def eval_line(repl_input: str) -> AST | LambdaError:
-    """Given REPL_INPUT, either evaluate as a directive, or else a raw
-    lambda term.
-
-    """
-
-    # Try to match the following pattern:
-    #
-    # NAME PARAMS...
-    #
-    # where NAME is the directive in question (e.g., "load").
-    _directive = re.match(r"(\.)(?P<name>.+?\b)(?P<params>.*)", repl_input)
-
-    if _directive is None:
-        # The program is this single line.
-        ast = eval_raw_term(repl_input)
-
-        if isinstance(ast, Exception):
-            return ast
-
-        return ast
-
-    name = _directive.group("name")
-    params = _directive.group("params").strip().split(" ")
-
-    # FIXME: right now, this just assumes that the directive
-    # always returns a program as the user_data field.
-    program = dtv.eval_directive(name, params)
-
-    if isinstance(program, LambdaError):
-        return program
-
-    value = eval_program(program)
-
-    if isinstance(value, Exception):
-        return value
-
-    return value
