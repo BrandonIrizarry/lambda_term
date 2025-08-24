a136b3eed3b563d08f52844b2e849cdf6e3b6acb
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-24 19:07:50 -0400

    test: 'select_first' prettifies correctly
    Here, I plan on abbreviating the selectors as "first", etc.

--- tests/gamma/test_prettify.py
+++ tests/gamma/test_prettify.py
@@ -10,6 +10,9 @@ F = term.new_abstraction
 A = term.new_application
 
 identity = F(N(0, 1))
+first = F(F(F(N(2, 3))))
+second = F(F(F(N(1, 3))))
+third = F(F(F(N(0, 3))))
 
 
 class TestPrettify(unittest.TestCase):
@@ -19,5 +22,14 @@ class TestPrettify(unittest.TestCase):
         pretty_id = repl.prettify(identity, [])
 
         mobj = re.fullmatch(r"\\(\w+)\.\1", pretty_id)
+
+        self.assertIsNotNone(mobj)
+
+    def test_first(self):
+        """Test 'select_first'."""
+
+        pretty_first = repl.prettify(first, [])
+
+        mobj = re.fullmatch(r"\\(\w+)\.\\(\w+)\.\\(\w+)\.\1", pretty_first)
 
         self.assertIsNotNone(mobj)
