68fd3aea6f0eee2576c3352aeaccefd459c4b5ad
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-23 21:43:17 -0400

    feat: make prettification testable by using a string return value
    So, instead of calling print within its recursive calls, it simply
    builds a string as those calls return, such that the final return
    value is the fully prettified lambda term.
    
    Note that the function has also been renamed to simply "prettify".

--- lbd/repl.py
+++ lbd/repl.py
@@ -34,48 +34,49 @@ See the TUTORIAL file accompanying this project for details.
 rword = RandomWord()
 
 
-def pretty_print_term_ast(ast: term.AST, env: list[str]):
-    """Pretty print AST as a human-readable lambda expression.
+def prettify(ast: term.AST, env: list[str]) -> str:
+    """Create a human-readable lambda expression from AST.
 
     Since the AST is constructed using DeBruijn indices, the original
     local variable names are discarded, and so synthetic names are used
     for the reconstructed human-readable expression.
 
+    Return the prettified version of AST as a string.
+
     """
+
     match ast["kind"]:
         case term.Term.NAME:
             fness = term.freeness(ast)
-
-            readable_name = "?"
 
             if fness < 0:
                 # An env_depth of -1 corresponds to TOS, -2 t one underneath, etc.
                 # Ex: index = 0 -> -1, index = 1 -> -2, etc.
                 idx = ast["index"]
                 env_depth = -(idx + 1)
-                readable_name = env[env_depth]
+
+                return env[env_depth]
             else:
                 free_sym = g.sym_get(fness)
 
                 if free_sym is None:
                     raise ValueError(f"Fatal: sym_get({fness}) failed")
 
-                readable_name = free_sym.label.upper()
+                return free_sym.label.upper()
 
-            print(readable_name, end="")
         case term.Term.ABSTRACTION:
             # Generate a random word to use as the function parameter.
             param: str = rword.word()
             env.append(param)
 
-            print("\\{}.".format(param), end="")
-            pretty_print_term_ast(ast["body"], env[:])
+            body = prettify(ast["body"], env[:])
+            return f"\\{param}.{body}"
+
         case term.Term.APPLICATION:
-            print("(", end="")
-            pretty_print_term_ast(ast["left"], env[:])
-            print(" ", end="")
-            pretty_print_term_ast(ast["right"], env[:])
-            print(")", end="")
+            left = prettify(ast["left"], env[:])
+            right = prettify(ast["right"], env[:])
+
+            return f"({left} {right})"
 
 
 def repl():
@@ -97,5 +98,5 @@ def repl():
             continue
 
         print(ast)
-        pretty_print_term_ast(ast, [])
+        print(prettify(ast, []))
         print()
