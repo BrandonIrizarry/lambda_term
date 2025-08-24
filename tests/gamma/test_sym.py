1154feb5ee8e27d836a221568a3913e0585bba1b
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-23 20:17:08 -0400

    test: parse expressions that use free symbols

---
+++ tests/gamma/test_sym.py
@@ -0,0 +1,38 @@
+import unittest
+
+import lbd.evaluate as evl
+import lbd.gamma as g
+import lbd.term as term
+
+N = term.new_name
+G = term.new_free_name
+F = term.new_abstraction
+A = term.new_application
+
+
+class TestDepth(unittest.TestCase):
+    """Test the depth field in name-ASTs.
+
+    Mainly, we want to see if free symbols keep their indices into
+    gamma consistent after beta-reduction, as well as to simply check
+    if a given lambda term with free symbols parses correctly.
+
+    """
+
+    def test_basic(self):
+        decl = "sym x y"
+        term = "(\\u.\\v.(u x) y)"
+
+        # Populate gamma.
+        evl.eval_raw_term(decl)
+
+        ast = evl.eval_raw_term(term)
+
+        x_index = g.gamma("x")
+        y_index = g.gamma("y")
+
+        assert x_index is not None
+        assert y_index is not None
+
+        self.assertEqual(ast, F(A(G(y_index, 1),
+                                  G(x_index, 1))))
