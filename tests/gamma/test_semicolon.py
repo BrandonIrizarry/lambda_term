454ec708f3d2efd7f64aa6689e36ca11ea5fb96e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-08-26 21:52:23 -0400

    test(fail): declaration then semicolon then expression

---
+++ tests/gamma/test_semicolon.py
@@ -0,0 +1,31 @@
+import unittest
+
+import lbd.error as err
+import lbd.evaluate as evl
+import lbd.gamma as g
+import lbd.tokenize as tkz
+from tests.aux import F, G, N, S
+
+
+class TestSemicolon(unittest.TestCase):
+    """Test using a semicolon to delimit terms occurring on the same
+    line in the REPL.
+
+    """
+
+    @unittest.expectedFailure
+    def test_simple_sym(self):
+        term = "sym u; \\x.u"
+        tokens = tkz.tokenize(term)
+
+        assert not isinstance(tokens, err.LambdaError)
+
+        ast = evl.eval_tokens(tokens)
+
+        assert not isinstance(ast, err.LambdaError)
+
+        expected = F(G(0, 1))
+
+        self.assertEqual(ast, expected)
+
+        g.clear_gamma()
