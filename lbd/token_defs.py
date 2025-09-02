83ef53b1a04af5f7601ab4ae5cd711e083475f0d
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-09-02 11:16:48 -0400

    feat: add a COMMENT token

--- lbd/token_defs.py
+++ lbd/token_defs.py
@@ -9,6 +9,7 @@ class Tk(enum.Enum):
     """
 
     ASSIGN = (enum.auto(), ":=")
+    COMMENT = (enum.auto(), "#")
     DOT = (enum.auto(), ".")
     ERROR = (enum.auto(), "")
     LAMBDA = (enum.auto(), "\\")
