4996252a7691977a67791e6e441cb9b586fbf826
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-10-03 13:09:53 -0400

    refactor: capture term match-patterns in appropriate names

--- lbd/beta.py
+++ lbd/beta.py
@@ -11,26 +11,26 @@ def _shift(ast: term.AST, amount: int, minimum: int) -> term.AST:
     """
 
     match ast:
-        case term.Name():
+        case term.Name() as name:
             if ast.index >= minimum:
-                return term.Name(ast.index + amount)
+                return term.Name(name.index + amount)
 
             return ast
 
-        case term.Abstraction():
-            new_body = _shift(ast.body, amount, minimum + 1)
+        case term.Abstraction() as abstr:
+            new_body = _shift(abstr.body, amount, minimum + 1)
 
             return term.Abstraction(new_body)
 
-        case term.Application():
-            new_left = _shift(ast.left, amount, minimum)
-            new_right = _shift(ast.right, amount, minimum)
+        case term.Application() as app:
+            new_left = _shift(app.left, amount, minimum)
+            new_right = _shift(app.right, amount, minimum)
 
             return term.Application(new_left, new_right)
 
-        case term.Assignment():
-            new_name = _shift(ast.name, amount, minimum)
-            new_value = _shift(ast.value, amount, minimum)
+        case term.Assignment() as assign:
+            new_name = _shift(assign.name, amount, minimum)
+            new_value = _shift(assign.value, amount, minimum)
 
             assert isinstance(new_name, term.Name)
 
@@ -61,34 +61,34 @@ def replace(ast: term.AST, argument: term.AST, target_index: int) -> term.AST:
     Return the modified AST."""
 
     match ast:
-        case term.Name():
-            if ast.index == target_index:
+        case term.Name() as name:
+            if name.index == target_index:
                 return argument
 
             return ast
 
-        case term.Abstraction():
+        case term.Abstraction() as abstr:
             new_argument = inc(argument, 0)
-            new_body = replace(ast.body, new_argument, target_index + 1)
+            new_body = replace(abstr.body, new_argument, target_index + 1)
 
             return term.Abstraction(new_body)
 
-        case term.Application():
-            new_left = replace(ast.left, argument, target_index)
-            new_right = replace(ast.right, argument, target_index)
+        case term.Application() as app:
+            new_left = replace(app.left, argument, target_index)
+            new_right = replace(app.right, argument, target_index)
 
             return term.Application(new_left, new_right)
 
-        case term.Assignment():
-            new_name = replace(ast.name, argument, target_index)
-            new_value = replace(ast.value, argument, target_index)
+        case term.Assignment() as assign:
+            new_name = replace(assign.name, argument, target_index)
+            new_value = replace(assign.value, argument, target_index)
 
             assert isinstance(new_name, term.Name)
 
             return term.Assignment(new_name, new_value)
 
-        case term.Empty():
-            return ast
+        case term.Empty() as empty:
+            return empty
 
         case _:
             raise ValueError(f"Fatal: invalid ast-kind: {ast}")
@@ -114,10 +114,10 @@ def beta_reduce(ast: term.AST) -> term.AST:
         # negative indices at this point are effectively impossible,
         # and are flagged with a ValueError, indicative of a fatal
         # flaw in the implementation.
-        case term.Name():
-            if (idx := ast.index) < 0:
+        case term.Name() as name:
+            if (idx := name.index) < 0:
                 raise ValueError(
-                    f"Fatal: name with negative index of {ast.index}")
+                    f"Fatal: name with negative index of {name.index}")
 
             sym = g.sym_get(idx)
 
@@ -136,15 +136,15 @@ def beta_reduce(ast: term.AST) -> term.AST:
 
             return result
 
-        case term.Abstraction():
-            return ast
+        case term.Abstraction() as abstr:
+            return abstr
 
-        case term.Application() as application:
-            beta_left = beta_reduce(ast.left)
+        case term.Application() as app:
+            beta_left = beta_reduce(app.left)
 
             match beta_left:
                 case term.Abstraction() as fn:
-                    arg = application.right
+                    arg = app.right
 
                     # Actual beta redux algorithm.
                     new_arg = inc(arg, 0)
@@ -154,30 +154,30 @@ def beta_reduce(ast: term.AST) -> term.AST:
                     return beta_reduce(new_replaced_body)
 
                 case _:
-                    beta_right = beta_reduce(application.right)
+                    beta_right = beta_reduce(app.right)
 
                     return term.Application(beta_left, beta_right)
 
-        case term.Assignment():
+        case term.Assignment() as assign:
             # The idea is to simply assign the AST to the given name,
             # which will be evaluated later as need be, under the
             # term.Name() case.
-            if (idx := ast.name.index) < 0:
+            if (idx := assign.name.index) < 0:
                 f"Fatal: name with negative index of {idx}"
 
             sym = g.sym_get(idx)
 
             if sym is None:
-                raise ValueError(f"Fatal: '{ast.name}' isn't a free symbol")
+                raise ValueError(f"Fatal: '{assign.name}' isn't a free symbol")
 
-            beta_value = ast.value
+            beta_value = assign.value
 
             g.sym_set(sym.label, beta_value)
 
             return beta_value
 
-        case term.Empty():
-            return ast
+        case term.Empty() as empty:
+            return empty
 
         case _:
             raise ValueError(f"Fatal: invalid ast-kind: {ast}")
